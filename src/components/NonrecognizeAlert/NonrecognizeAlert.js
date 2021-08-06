06941e544645f3bffaf268a81377ab5d4cc89162
dogukan_901 <doki9706@gmail.com>
2021-08-06 19:21:44 +0300

    alert added continuing styling

--- src/components/NonrecognizeAlert/NonrecognizeAlert.js
+++ src/components/NonrecognizeAlert/NonrecognizeAlert.js
@@ -1,32 +1,57 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Alert, AlertTitle } from '@material-ui/lab';
+import { Box } from '@material-ui/core'
+import { OuterNonrecognized } from './NonrecognizeAlertstyles';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
+    alignItemsAndJustifyContent: {
+        width: 500,
+        height: 80,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'pink',
+    },
     root: {
         width: '100%',
         '& > * + *': {
-            // marginTop: theme.spacing(2),
+            marginTop: theme.spacing(2),
             display: "flex",
-            alignItems: "center",
+            align: "center",
             background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
         },
     },
-});
+}));
 
 
 const NonrecognizeAlert = () => {
 
     const classes = useStyles();
     return (
-        <div className={classes.root}>
+        <React.Fragment >
+            <OuterNonrecognized>
+                {/* <Box
+                    display="flex"
+                    width={500} height={80}
+                    bgcolor="lightgreen"
+                    alignItems="center"
+                    justifyContent="center"
+                >
+                    2. Box (alignItems and justifyContent)
+                </Box> */}
+                <div>
 
-            <Alert severity="error" >
-                <AlertTitle>Yüzünüzü Tanıyamadık :(</AlertTitle>
-                <strong>Lütfen kişisel bilgiler formunu doldurunuz.</strong>
-            </Alert>
+                    <Alert severity="error" >
+                        <AlertTitle alignItems="center" justifyContent="center">Yüzünüzü Tanıyamadık :(</AlertTitle>
+                        <strong>Lütfen kişisel bilgiler formunu doldurunuz.</strong>
+                    </Alert>
 
-        </div >
+                </div >
+            </OuterNonrecognized>
+
+        </React.Fragment>
+
 
 
 
--- src/components/NonrecognizeAlert/NonrecognizeAlertstyles.js
+++ src/components/NonrecognizeAlert/NonrecognizeAlertstyles.js
@@ -1,2 +1,8 @@
 import styled from 'styled-components';
 
+
+export const OuterNonrecognized = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
